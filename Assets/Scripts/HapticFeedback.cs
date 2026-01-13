53fe980f52255fd43ed600bf5f2b0954c27fca44
John Bacho <Johnbacho50@gmail.com>
2026-01-13 14:49:46 -0500

    UX / UI Enhancement v1.1.1-Beta
    Many improvements: instructions have reduced size and improved wording #39. Controllers have been rotated upwards by 12 degrees #38. Fixed an issue that caused the controller to trigger haptic hover during parlay condition. Improved haptic feedback manager.

--- Assets/Scripts/HapticFeedback.cs
+++ Assets/Scripts/HapticFeedback.cs
@@ -1,12 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
-public class HapticFeedback : MonoBehaviour
+public class HapticsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void TriggerHaptic()
+
+    public void CustomTriggerHaptic(float amplitude, float duration)
     {
-        sxr.SendHaptic(0.5f, 1.0f, true, 0);
+        InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        if (device.isValid && device.TryGetHapticCapabilities(out HapticCapabilities capabilities))
+        {
+            if (capabilities.supportsImpulse)
+                device.SendHapticImpulse(0, amplitude, duration);
+        }
+    }
+
+        public void TriggerHaptic()
+    {
+        float amplitude = 0.5f;
+        float duration = 0.1f;
+        InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+        if (device.isValid && device.TryGetHapticCapabilities(out HapticCapabilities capabilities))
+        {
+            if (capabilities.supportsImpulse)
+            {
+                device.SendHapticImpulse(0, amplitude, duration);
+            }
+        }
+    }
+
+    public void HapticIncreaseBet()
+    {
+        CustomTriggerHaptic(0.7f, 0.1f);
+    }
+
+    public void HapticDecreaseBet()
+    {
+        CustomTriggerHaptic(0.5f, 0.15f);
+    }
+
+    public void HapticUntoggle()
+    {
+        CustomTriggerHaptic(0.5f, 0.05f);
+        Invoke(nameof(SecondUntogglePulse), 0.07f);
+    }
+
+    private void SecondUntogglePulse()
+    {
+        CustomTriggerHaptic(0.5f, 0.05f);
     }
 }
+
