fda509fd58ca9521c8bb30f10f528f54fbd050b1
John Bacho <Johnbacho50@gmail.com>
2026-02-06 17:40:34 -0500

    Trials redistributed
    updated frequency of wins losses and near misses to match research papers. Also removed dynamic loss from parlay. #65

--- Assets/Scripts/ParlayHandler.cs
+++ Assets/Scripts/ParlayHandler.cs
@@ -222,10 +222,6 @@ public class ParlayHandler : MonoBehaviour, ManageWallet
     private IEnumerator ResolveBet(List<bool> LegWins)
     {
         float Payout = 0f;
-        if(LegWins.Count(b => b) != decimalOddsList.Count && decimalOddsList.Count != LegWins.Count(b => b)+1)
-        {
-            LegWins = dynamicLoss(LegWins);
-        }
         CardManager.AnimateCardsColor(LegWins);
 
         yield return new WaitForSeconds(seconds);
@@ -417,64 +413,6 @@ public class ParlayHandler : MonoBehaviour, ManageWallet
                 togglePressInteractables[i].OnStatsDeselected();
             }
         }
-    }
-    private List<bool> dynamicLoss(List<bool> legWins)
-    {    
-        int targetWins = legWins.Count(b => b);
-        List<bool> newLegWins = new List<bool>();
-        for (int i = 0; i < decimalOddsList.Count; i++)
-        {
-            float probability = 1f / decimalOddsList[i];
-            newLegWins.Add(Random.value < probability ? true : false);
-        }
-        
-        int currentWins = newLegWins.Count(b => b);
-
-        if (currentWins > targetWins)
-        {
-            int toFlip = currentWins - targetWins;
-            List<int> winIndices = new List<int>();
-            
-            for (int i = 0; i < newLegWins.Count; i++)
-            {
-                if (newLegWins[i])
-                    winIndices.Add(i);
-            }
-
-            
-            for (int i = 0; i < toFlip; i++)
-            {
-                int randomIndex = Random.Range(i, winIndices.Count);
-                int temp = winIndices[i];
-                winIndices[i] = winIndices[randomIndex];
-                winIndices[randomIndex] = temp;
-                
-                newLegWins[winIndices[i]] = false;
-            }
-        }
-        else if (currentWins < targetWins)
-        {
-            int toFlip = targetWins - currentWins;
-            List<int> lossIndices = new List<int>();
-            
-            for (int i = 0; i < newLegWins.Count; i++)
-            {
-                if (!newLegWins[i])
-                    lossIndices.Add(i);
-            }
-            
-            for (int i = 0; i < toFlip; i++)
-            {
-                int randomIndex = Random.Range(i, lossIndices.Count);
-                int temp = lossIndices[i];
-                lossIndices[i] = lossIndices[randomIndex];
-                lossIndices[randomIndex] = temp;
-                
-                newLegWins[lossIndices[i]] = true;
-            }
-        }
-        
-        return newLegWins;
     }
 
     public float GetWallet()
