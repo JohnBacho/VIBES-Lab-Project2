49b7e37324556af5184213e91ce63a8595f1cb0c
John Bacho <Johnbacho50@gmail.com>
2026-03-01 15:54:11 -0500

    Added in instructions
    Parlay instructions added last thing to do is add in some props.

--- Assets/ParlayInstructions.cs
+++ Assets/ParlayInstructions.cs
@@ -6,22 +6,32 @@ using TMPro;
 
 public class ParlayInstructions : MonoBehaviour
 {
-    [SerializeField] private GameObject instructionSceneParlay;
-    [SerializeField] private TextMeshPro textParlayInstructions;
+    [SerializeField] private GameObject Pt1InstructionsPanel;
+    [SerializeField] private GameObject Pt2InstructionsPanel;
+    [SerializeField] private GameObject Pt3InstructionsPanel;
+
 
     public IEnumerator ShowParlayInstructions()
     {
-        instructionSceneParlay.SetActive(true);
-        textParlayInstructions.text = "PlaceHolder";
+        Pt1InstructionsPanel.SetActive(true);
         yield return new WaitForSeconds(3f);
         yield return new WaitUntil(() => sxr.GetTrigger());
+        Pt1InstructionsPanel.SetActive(false);
         yield return Pt2(); 
     }
 
     private IEnumerator Pt2()
     {
-        textParlayInstructions.text = "pt2 PlaceHolder";
-        yield return new WaitForSeconds(2f);
-        instructionSceneParlay.SetActive(false);
+        Pt2InstructionsPanel.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        Pt2InstructionsPanel.SetActive(false);
+        yield return Pt3(); 
+    }
+    private IEnumerator Pt3()
+    {
+        Pt3InstructionsPanel.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        Pt3InstructionsPanel.SetActive(false);
+        yield return Pt3(); 
     }
 }
